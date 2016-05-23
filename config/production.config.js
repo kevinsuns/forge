ef3c0b80fae8fd28a2cdcf00684a71a7b3132a9e
Philippe Leefsma <philippe.leefsma@autodesk.com>
2016-05-23 20:10:57 +0200

    update

--- config/production.config.js
+++ config/production.config.js
@@ -1,14 +1,30 @@
 
+var OAUTH_VERSION = 'v1';
+var DM_PROJECT_VERSION = 'v1';
+
 module.exports = {
 
   clientConfig: {
-    host: '',
-    port: 443
+    host: 'localhost',
+    port: 3000
   },
 
   serverConfig: {
-    port: process.env.PORT || process.env.NODE_PORT || 3000,
-    LMV_CONSUMERKEY: process.env.LMV_CONSUMERKEY,
-    LMV_CONSUMERSECRET: process.env.LMV_CONSUMERSECRET
+    port: 3000,
+    baseUrl: 'https://developer-stg.api.autodesk.com',
+    redirectUrl: 'https://forge.herokuapp.com/api/auth/callback',
+    authenticationUrl: '/authentication/' + OAUTH_VERSION + '/authorize',
+    accessTokenUrl: '/authentication/' + OAUTH_VERSION + '/gettoken',
+    scope: [
+      'data:create',
+      'data:read',
+      'data:write',
+      'bucket:read',
+      'bucket:create'
+    ],
+    credentials: {
+      ConsumerKey: process.env.LMV_CONSUMERKEY,
+      ConsumerSecret: process.env.LMV_CONSUMERSECRET
+    }
   }
 }
