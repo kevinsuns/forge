407e2d07d3437571867a4745ca55199deff7d40d
Philippe Leefsma <philippe.leefsma@autodesk.com>
2016-05-25 17:51:34 +0200

    update

--- src/server/api/endpoints/auth.js
+++ src/server/api/endpoints/auth.js
@@ -40,12 +40,16 @@ module.exports = function() {
         'grant_type': 'authorization_code',
         'redirect_uri': config.redirectUrl
       },
-      function (e, access_token, refresh_token, results) {
+      function (err, access_token, refresh_token, results) {
 
         try {
-          req.session.token = access_token;
-          req.session.cookie.maxAge = parseInt(results.expires_in) * 60; // same as access_token
 
+          if(results){
+
+            req.session.token = access_token;
+            req.session.cookie.maxAge = parseInt(results.expires_in) * 60; // same as access_token
+          }
+          
           res.end('<script>window.opener.location.reload(false);window.close();</script>');
         }
         catch(ex){
