b6596d938ab8f5b75cc5fc4dd03226c9e35a19db
Philippe Leefsma <philippe.leefsma@autodesk.com>
2016-07-23 00:27:20 +0200

    update

--- src/server/api/services/OssSvc.js
+++ src/server/api/services/OssSvc.js
@@ -15,7 +15,7 @@ export default class OssSvc extends BaseSvc {
 
     super(opts)
 
-    this.loadBucketSettings()
+    //this.loadBucketSettings()
   }
 
   /////////////////////////////////////////////////////////////////
